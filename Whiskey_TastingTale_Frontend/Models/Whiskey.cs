3771cf4546b742c5b7359f00f217d586309ac8eb
YoorimChoi <cyl578057@gmail.com>
2024-10-01 00:17:38 +0900

    [feat] Backend Whiskeys API 변경 사항 반영 - rating 포함
     - Backend :  Whiskeys API에 Rating 정보 포함하여 응답으로 변경
     - 변경된 인터페이스에 맞게 Model 수정
     - rating이 필요한 페이지, rating 쓸 수 있도록 수정

--- Whiskey_TastingTale_Frontend/Models/Whiskey.cs
+++ Whiskey_TastingTale_Frontend/Models/Whiskey.cs
@@ -5,12 +5,17 @@ namespace Whiskey_TastingTale_Backend.Model
     public class Whiskey
     {
         [Key]
-        public int whiskey_id {  get; set; }  
-        public string whiskey_name { get; set; }
-        public string img_index { get; set; }
+        public int whiskey_id { get; set; }
+        public string? whiskey_name { get; set; }
+        public string? img_index { get; set; }
         public double? alcohol_degree { get; set; }
         public string? maker { get; set; }
         public string? details { get; set; }
         public string? page_url { get; set; }
+        public double rating { get; set; }
+        public int review_count { get; set; }
+
+        public DateTime created_date { get; set; }
+        public DateTime updated_date { get; set; }
     }
 }
