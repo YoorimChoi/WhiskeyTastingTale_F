7ec52b070e8912f462029df6d07e9eec0ec69c7f
YoorimChoi <cyl578057@gmail.com>
2024-10-04 10:31:45 +0900

    [refactor] 뒤로가기 버튼을 위한 PageState 생성 및 적용
     - 기존 : 뒤로가기 버튼 시, JS 를 이용하여 수행. 리뷰 작성 후 리뷰 목록 페이지에서 뒤로가기 하면 다시 리뷰 작성 페이지로 가는 문제
     - 변경 : Singleton으로 PageState 생성, 이전 페이지 정보를 기록하여 해당 기록을 통해 뒤로가기 버튼 기능 사용

--- Whiskey_TastingTale_Frontend/Program.cs
+++ Whiskey_TastingTale_Frontend/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddSingleton<DetailsViewModel>();
 
 
 builder.Services.AddSingleton<UserState>();
+builder.Services.AddScoped<PageState>();
 builder.Services.AddSingleton<WhiskeyState>();
 
 var app = builder.Build();
---
+++ Whiskey_TastingTale_Frontend/Services/PageState.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Whiskey_TastingTale_Backend.Model;
+
+namespace Whiskey_TastingTale_Frontend.Services
+{
+    public class PageState
+    {
+        private string priviousPage = String.Empty; 
+        public string PriviousPage
+        {
+            get
+            {
+                return priviousPage;
+            }
+            set
+            {
+                priviousPage = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+    }
+
+
+       
+}
