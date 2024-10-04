7040deaf630dd4346c00a2f1226d7a5e3b8d7537
YoorimChoi <cyl578057@gmail.com>
2024-10-04 09:54:43 +0900

    [refactor] LoginDTO 생성 및 적용
     - 기존 : login 시 token만 서버로 부터 응답 받음
     - 변경 : login 시 token 과 user에 대한 세부 정보를 응답 받음

--- Whiskey_TastingTale_Frontend/Models/Login.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Whiskey_TastingTale_Backend.Model
-{
-    public class LoginResult
-    {
-        public required string token {  get; set; }
-    }
-}
--- Whiskey_TastingTale_Frontend/Services/UserState.cs
+++ Whiskey_TastingTale_Frontend/Services/UserState.cs
@@ -3,7 +3,11 @@
     public class UserState
     {
         private string token = null;
-        private string email = null; 
+        private string email = null;
+        private int user_id = -1;
+        private string nickname = null;
+        private string role = null; 
+
         public string Token
         {
             get
@@ -30,9 +34,57 @@
             }
         }
 
+        public int UserId
+        {
+            get
+            {
+                return user_id;
+            }
+            set
+            {
+                user_id = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+            set
+            {
+                role = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+                NotifyStateChanged();
+            }
+        }
+
         public event Action OnChange;
 
         private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void init()
+        {
+            Token = null;
+            UserId = -1;
+            Role = null;
+            Nickname = null;
+            Email = null; 
+        }
     }
 
 }
