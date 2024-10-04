7b83ecce0ece5f08ae677e02e0080fcbaf445586
YoorimChoi <cyl578057@gmail.com>
2024-10-04 11:26:25 +0900

    [feat] 리뷰 작성 기능 추가

--- Whiskey_TastingTale_Frontend/ViewModels/DetailsViewModel.cs
+++ Whiskey_TastingTale_Frontend/ViewModels/DetailsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.JSInterop;
+using Newtonsoft.Json;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Whiskey_TastingTale_Backend.API.DTOs;
@@ -9,15 +10,20 @@ namespace Whiskey_TastingTale_Frontend.ViewModels
 {
     public class DetailsViewModel : INotifyPropertyChanged
     {
-        private readonly WhiskeyState _whiskeyState; 
-        public DetailsViewModel(WhiskeyState whiskeyState)
+        private readonly WhiskeyState _whiskeyState;
+        private readonly UserState _userState;
+
+        public DetailsViewModel(WhiskeyState whiskeyState, UserState userState)
         {
             _whiskeyState = whiskeyState;
+            _userState = userState;
         }
 
         private Whiskey selected = new Whiskey();
         private List<ReviewUserDTO> reviews = new List<ReviewUserDTO>();
-        private ReviewUserDTO firstReview = new ReviewUserDTO(); 
+        private ReviewUserDTO firstReview = new ReviewUserDTO();
+        private int selectedRating = 0;
+        private string review = String.Empty; 
 
         public Whiskey Selected
         {
@@ -49,6 +55,25 @@ namespace Whiskey_TastingTale_Frontend.ViewModels
             }
         }
 
+        public int SelectedRating
+        {
+            get { return selectedRating; }
+            set
+            {
+                selectedRating = value;
+                OnPropertyChanged(nameof(SelectedRating));
+            }
+        }
+
+        public string Review
+        {
+            get { return review; }
+            set
+            {
+                review = value;
+                OnPropertyChanged(nameof(Review));
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName)
@@ -59,6 +84,12 @@ namespace Whiskey_TastingTale_Frontend.ViewModels
         public async Task LoadData()
         {
             Selected = _whiskeyState.Selected;
+            var whiskey = await RestApiHelper.Get(RestApiHelper.server_uri + "Whiskey/" + Selected.whiskey_id);
+            if (whiskey != null)
+            {
+                Selected = JsonConvert.DeserializeObject<Whiskey>(whiskey.ToString());
+            }
+
             var response = await RestApiHelper.Get(RestApiHelper.server_uri + "Review/whiskey/" + Selected.whiskey_id);
 
             if (response != null)
@@ -66,6 +97,17 @@ namespace Whiskey_TastingTale_Frontend.ViewModels
                 Reviews = JsonConvert.DeserializeObject<List<ReviewUserDTO>>(response.ToString());
                 FirstReview = Reviews.FirstOrDefault(); 
             }
+        }
+
+        public async Task AddReview()
+        {
+            var response = await RestApiHelper.Post(RestApiHelper.server_uri + "Review/", new Review
+            {
+                user_id = _userState.UserId,
+                whiskey_id = Selected.whiskey_id,
+                rating = SelectedRating, 
+                review_text = Review
+            });
         }
     }
 
