47b292a8633a6fd1f3d48f731fc66c15fefdba90
YoorimChoi <cyl578057@gmail.com>
2024-10-29 18:43:54 +0900

    [feat] 알림 기능 추가
     - 로그인 시, role이 User 이면 UserLayout 으로 변경
       > 기존 : Admin, Main 두가지 Layout 만으로 구성 (관리자로 로그인 시 Admin, 그외 Main)
       > 변경 : Admin, Main, User로 수정  (관리자로 로그인 시 Admin, User로 로그인 시 User, 로그인 안했을 때는 Main)
     - UserLayout 인 경우 Drawer를 통해 알림 도시, 읽음 처리, 삭제 처리 가능
     - UserLayout 인 경우 NotificationViewModel 생성 되면서 SiganlR 서비스도 생성 및 시작
       > 서버와 연결 맺을 때, user id 를 이용해 user 식별 가능하도록 함
     -  Backend 에서 추가된 알림 기능과 UI 연동

---
+++ Whiskey_TastingTale_Frontend/Models/NotificationPageDTO.cs
@@ -0,0 +1,14 @@
+﻿using Whiskey_TastingTale_Backend.Data.Entities;
+
+namespace Whiskey_TastingTale_Backend.API.DTOs
+{
+    public class NotificationPageDTO
+    {
+        public List<Notification> notifications { get; set; }
+        public int page { get; set; }
+        public int pageSize { get; set; }
+        public int totalCount { get; set; }
+        public int totalPages { get; set; }
+    }
+
+}
